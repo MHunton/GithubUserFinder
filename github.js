05bfa0f8844ba223bf2001f3c2162cefbe841e6e
MHunton <mhunton@protonmail.com>
2021-08-06 18:23:15 +0100

    first commit

---
+++ github.js
@@ -0,0 +1,16 @@
+class Github {
+    constructor() {
+        this.client_id = '855a11e55f5b0c46b28e';
+        this.client_secret = '404b6a4e7445612836e03db151e019c3e1f5f6c3';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+
+        return {
+            profile
+        }
+    }
+}
